2a5a3d7e751fc1f3ac1f9e713fb68fc486a5c30d
bieanbarapradana <bieanbarapradana@gmail.com>
2018-08-12 17:26:22 +0700

    Built Reveal Scroll
    needs, refractoring code

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,6 @@
 
 // JavaScript
-window.sr = ScrollReveal(); 
+ 
 
 // sr.reveal('h1', {
 //     delay: 0,
@@ -19,3 +19,42 @@ function showNav(){
     }
 
 }
+
+window.sr = ScrollReveal();
+
+sr.reveal('#header img.phone',{
+    duration: 1000,
+    origin: 'bottom',
+    distance: '200px'    
+});
+sr.reveal('#features div.package',{
+    delay: 200,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'    
+});
+sr.reveal('#features div.delivery',{
+    delay: 400,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'    
+});
+sr.reveal('#features div.return',{
+    delay: 600,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'    
+});
+sr.reveal('#discount .offer',{
+    delay: 600,
+    duration: 500,
+    origin: 'right',
+    distance: '200px'    
+});
+sr.reveal('#discount img.double-phone',{
+    delay: 600,
+    duration: 500,
+    origin: 'left',
+    distance: '200px'    
+});
+
