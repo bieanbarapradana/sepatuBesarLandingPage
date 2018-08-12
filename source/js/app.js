f9d40656a722465c3cd53b7fd84eb58e823ed3a5
bieanbarapradana <bieanbarapradana@gmail.com>
2018-08-12 14:28:48 +0700

    Built out responsive navigation & refract code
    Needs refract code and reveal scroll

--- source/js/app.js
+++ source/js/app.js
@@ -8,3 +8,14 @@ window.sr = ScrollReveal();
 //     origin: 'bottom',
 //     distance: '100px' 
 // });
+
+function showNav(){
+    console.log("this is working");
+    var x = document.getElementById("responsive-nav");
+    if (x.className === "responsive-nav") {
+        x.className += " unfold";
+    } else {
+        x.className = "responsive-nav";
+    }
+
+}
